d875ac938544a0ea736d10a0f1035e487eed59e2
thanneman <thanneman1@gmail.com>
2020-03-11 18:05:18 -0700

    created react app

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import PrivateRoute from './components/utils/PrivateRoute'
+//import PublicOnlyRoute from './components/utils/PublicOnlyRoute'
+//import Login from './routes/Login'
+//import Signup from './routes/Signup'
+//import Dashboard from './routes/Dashboard'
+//import './App.css'
+
+export default class App extends Component {
+    
+    
+    render() {
+      return (
+        <div className="App">
+            <Switch>
+              <Route exact path={'/'} component={Login} />
+              <Route path={'/signup'} component={Signup} />
+              <PrivateRoute path={'/dashboard'} component={Dashboard} />
+              <PrivateRoute path={'/newgame'} component={NewGame} />
+              <PrivateRoute path={'/stats'} component={Stats} />
+            </Switch>
+        </div>
+      )
+    }
+
+
+}
---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+    API_ENDPOINT: 'Update-This',
+    //API_ENDPOINT: 'http://localhost:8000/api',
+    TOKEN_KEY: 'oneliner-client-auth-token',
+  }
